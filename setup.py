cc64ca2d93f8bb65217494ba476142507511eb35
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-12-24 10:41:05 +0800

    Also relax pil

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ root_path = Path(__file__).parent
 install_requires = [
     'nml==0.8.0',
     'numpy>=1.26.0',
-    'Pillow==12.0.0',
+    'Pillow>=9.4.0',
     'typeguard==4.4.4',
     'lark==1.3.0',
 ]
